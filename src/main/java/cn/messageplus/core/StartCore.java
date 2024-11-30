01e7458368f185371843d77cf132a6412e4511c0
mo <1827568954@qq.com>
2024-12-01 00:21:12 +0800

    更新测试客户端类

--- src/main/java/cn/messageplus/core/StartCore.java
+++ src/main/java/cn/messageplus/core/StartCore.java
@@ -1,10 +1,58 @@
 package cn.messageplus.core;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class StartCore {
     static {
-
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    // 用来判断是不是 读空闲时间过长，或 写空闲时间过长
+                    // 5s 内如果没有收到 channel 的数据，会触发一个事件
+                    ch.pipeline().addLast(new IdleStateHandler(5, 0, 0));
+                    // ChannelDuplexHandler 可以同时作为入栈和出栈处理器
+                    ch.pipeline().addLast(new ChannelDuplexHandler() {
+                        // 用来触发特殊事件
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+                            IdleStateEvent event = (IdleStateEvent) evt;
+                            // 触发了读空闲事件
+                            if (event.state() == IdleState.READER_IDLE) {
+                                System.out.println("已经 5s 没有读到数据了");
+                                log.debug("已经 5s 没有读到数据了");
+                                ctx.channel().close();
+                            }
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
     }
 }
